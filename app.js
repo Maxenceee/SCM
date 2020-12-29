8def70c9206715e74aa4246126934064afc0737a
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-29 18:24:24 +0100

    adding index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get("/home", function(request, response){
-    response.send("SCM!")
+    response.send(document.getElementById('main_page').innerHTML)
 })
 
 app.listen(3000)

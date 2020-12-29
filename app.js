f594db2e93f02625b6524c4b56ec50044f99af88
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-29 18:27:57 +0100

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get("/home", function(request, response){
-    response.send(document.getElementById('main_page').innerHTML)
+    response.send() = document.getElementById('main_page').innerHTML
 })
 
 app.listen(3000)

8e3a3ddb25c1cbe913e1482ca95bb5826698f531
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-30 15:44:10 +0100

    new t

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const app = express()
 
 app.get("/home", function(request, response){
     //response.send('main_page')
-    window.navigate("main_page.html");
+    var http = require('http'),
+    fs = require('fs');
+
+    fs.readFile('./main_page.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }       
+        http.createServer(function(request, response) {  
+            response.writeHeader(200, {"Content-Type": "text/html"});  
+            response.write(html);  
+            response.end();  
+        }).listen(3000);
+    });
 })
 
 app.listen(3000)
+

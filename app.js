b32591656a8c0c0c5707fd05b17b7e041e5534ab
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-30 15:39:00 +0100

    test app

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express')
 const app = express()
 
 app.get("/home", function(request, response){
-    response.send('main_page')
+    //response.send('main_page')
+    window.navigate("main_page.html");
 })
 
 app.listen(3000)

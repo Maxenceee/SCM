7725a8fe62641b083108288346e2426bd41b861b
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-30 16:09:09 +0100

    n

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/home', function(request, response){
-    response.sendFile('./home/bitnami/SCM/MainPage/main_page.html');
+    response.sendFile('home/bitnami/SCM/MainPage/main_page.html');
 });
 
 /*

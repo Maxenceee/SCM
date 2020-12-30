b7b7bf3b9fcc2a59f10e04d1908c1747e1cc49a8
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2020-12-30 16:24:35 +0100

    new update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require('express')
 
 const app = express()
@@ -6,6 +6,7 @@ const app = express()
 app.get('/home', function(request, response){
     response.sendFile('./MainPage/main_page.html');
 });
+*/
 
 /*
 app.get("/home", function(request, response){
@@ -24,7 +25,25 @@ app.get("/home", function(request, response){
         }).listen(3000);
     });
 })
+app.listen(3000)
 */
 
-app.listen(3000)
-
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./MainPage/main_page.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(3000);

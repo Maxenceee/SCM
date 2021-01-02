25a5c4c7f99f56bcbea3c56949d4db0f9efa7d26
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2021-01-02 19:17:21 +0100

    new update

--- app.js
+++ app.js
@@ -25,25 +25,3 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 })
-
-/*
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./MainPage/main_page.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
- 
-http.createServer(handleRequest).listen(3000);
-*/
